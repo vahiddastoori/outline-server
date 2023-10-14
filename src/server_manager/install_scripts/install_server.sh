2c1fb92de886a96c1b76c5146cc452897000f50e
vahiddsy <dastoory@rajman.org>
2023-10-14 18:46:16 +0330

    update install server remove docker-compose

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -171,9 +171,6 @@ function install_docker() {
     fetch https://get.docker.com/ | sh
   ) >&2
   usermod -aG docker $USER
-  curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
-  docker-compose --version
 }
 
 function start_docker() {
